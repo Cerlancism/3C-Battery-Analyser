cf1927bfb7a491974d2f2f8861ccacbec65acb92
Cerlancism <scheduledx@gmail.com>
2020-07-12 20:46:34 +0800

    Add system command line

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
 
 namespace _3C_Battery_Analyser
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rootCommand = new RootCommand
+            {
+                new Option<string>(
+                    "--file",
+                    getDefaultValue: () => "history.txt",
+                    description: "History File"),
+            };
+
+            rootCommand.Description = "3C Battery history analyser";
+
+            // Note that the parameters of the handler method are matched according to the names of the options
+            rootCommand.Handler = CommandHandler.Create<string>(Analyse);
+
+            // Parse the incoming args and invoke the handler
+            return rootCommand.InvokeAsync(args).Result;
+        }
+
+        private static void Analyse(string file)
+        {
+            file = Path.GetFullPath(file);
+            Console.WriteLine($"File: {file}");
         }
     }
 }
