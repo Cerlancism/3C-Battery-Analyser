760b02ea335da8872d05854520314a861c3ce6ad
Cerlancism <scheduledx@gmail.com>
2020-07-12 20:40:10 +0800

    Git init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3C_Battery_Analyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
